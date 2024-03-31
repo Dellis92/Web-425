c227181a0c947db95b61f0295356a76301eb4aec
DeVonte <devonte.ellis@gmail.com>
2024-03-30 22:41:45 -0500

    2.2

---
+++ week-2/my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BroserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
